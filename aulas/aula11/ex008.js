23259a71867c452afcefaaeebed120a43cfe45f2
Manoel-Pires <manoelpnj69@gmail.com>
2023-04-18 18:49:47 -0300

    estudo de condicionais

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){
+    console.log('Voce ultrapassou o limite de velocidade. Multado!!')
+}
+console.log('dirija sempre usando o cinto de segurança!')
